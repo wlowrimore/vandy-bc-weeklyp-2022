55198859534a2ee270f48bdb10e55919e4286d69
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-01-20 12:59:51 -0600

    added time variable and changed time display function

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -12,9 +12,79 @@
 // WHEN I refresh the page
 // THEN the saved events persist
 
-// Current Date
-function getDate() {
-  var today = moment().format('LLLL');
-  var el = document.getElementById("currentDay").innerHTML= today; 
+    
+
+// time slots
+var dailyTimes = [
+  {
+      id: "0",
+      hour: "9",
+      time: "09",
+      meridiem: "am",
+      reminder: ""
+  },
+  {
+      id: "1",
+      hour: "10",
+      time: "10",
+      meridiem: "am",
+      reminder: ""
+  },
+  {
+      id: "2",
+      hour: "11",
+      time: "11",
+      meridiem: "am",
+      reminder: ""
+  },
+  {
+      id: "3",
+      hour: "12",
+      time: "12",
+      meridiem: "pm",
+      reminder: ""
+  },
+  {
+      id: "4",
+      hour: "1",
+      time: "13",
+      meridiem: "pm",
+      reminder: ""
+  },
+  {
+      id: "5",
+      hour: "2",
+      time: "14",
+      meridiem: "pm",
+      reminder: ""
+  },
+  {
+      id: "6",
+      hour: "3",
+      time: "15",
+      meridiem: "pm",
+      reminder: ""
+  },
+  {
+      id: "7",
+      hour: "4",
+      time: "16",
+      meridiem: "pm",
+      reminder: ""
+  },
+  {
+      id: "8",
+      hour: "5",
+      time: "17",
+      meridiem: "pm",
+      reminder: ""
+  },
+]
+
+// today's date
+function getTodaysDate() {
+  var curDate = moment().format('dddd, MMMM Do, YYYY');
+  $("#currentDay").text(curDate);
 }
-getDate();    
+getTodaysDate();
+
